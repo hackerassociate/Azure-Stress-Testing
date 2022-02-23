9a17689fbbdf984afe13246ad18775c5a60ddaa9
hackerassociate <62351511+hackerassociate@users.noreply.github.com>
2022-02-23 18:33:38 +0530

    Add files via upload

---
+++ Azure_Stress_Testing.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+#Author Harshad Shah 
+
+echo -e "\nWelcome to  Global Offensive Hacker Associate Community."
+
+echo -e "\n";  sudo apt-get update ; sudo apt upgrade ; sudo apt install stress 
+
+
+echo -e "\nBe Patient, H-Bot is Generating load on server."
+
+
+echo -e "\n"; sudo stress --cpu 100 
+
+
